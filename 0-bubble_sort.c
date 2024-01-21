9724819be79e69afbe7f55ec3a51626b295063b4
LINUSOBURA <linusobura@gmail.com>
2024-01-21 22:19:30 +0300

    initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to sort array using bubble sort method
+ * @array: pointer to an array to be sorted
+ * @size: size of the array
+ * Code by Linus Obura
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int i, k, j;
+
+	for (k = 0; k <= size - 1; k++)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				j = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = j;
+				print_array(array, size);
+			}
+		}
+	}
+}
